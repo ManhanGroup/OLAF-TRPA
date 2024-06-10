3e8619ccde960f3b500cb7a41e73772dbc17dac8
purpleplum <ya@manhangroup.com>
2024-06-10 10:14:09 -0700

    Add Market Segmentation and More

--- allocation.py
+++ allocation.py
@@ -48,6 +48,8 @@ class model:
         self.zone_df = pd.read_csv(self.config['zonal_data'])
         self.id = self.config['geo_id']
         self.zone_df.set_index(self.id,drop=False,inplace=True)
+        self.neighbors=np.load(self.config['neighbors'])
+        self.year=self.config['year']
         self.land_uses = self.config['land_uses']
         self.draws = self.config['draws']
         self.qry_developable=self.config['filter_Undevelopable']
@@ -123,11 +125,48 @@ class model:
         # this is a function for custom updates on the dataframe before/after allocation
         for op in self.config['update_block']:
             self.zone_df.eval(op, inplace=True)
+    
+    def updateneighbor(self):
+       # Load neighbors from the NPY file
+       # remove the address/point itself from the array because it itself is its nearest neighbour
+       neighbors = self.neighbors[:, 1:]
+       self.zone_df.loc[(self.zone_df.BUILDING_SQFT>0), 'bldage']=self.zone_df.loc[(self.zone_df.BUILDING_SQFT>0),'YEAR_BUILT'].map(lambda x: self.year-x)
+
+       #Create a dataframe to store intermediate columns
+       nei_parcel = pd.DataFrame(index=self.zone_df.index)
+       ncols=['neighbors_age','neighbors_totalunits','neighbors_BUILDING_SQFT','neighbors_PARCEL_SQFT','neighbors_per_built']
+       for c in ncols:
+           nei_parcel[c]=None
+
+       #calculate neiboring parcels total building ages
+       nei_parcel['neighbors_age']= [self.zone_df['bldage'].iloc[n].sum() for n in neighbors]
+
+       #calculate neiboring parcels total building sqft and total parcel sqft
+       nei_parcel['neighbors_BUILDING_SQFT']= [self.zone_df['BUILDING_SQFT'].iloc[n].sum() for n in neighbors]
+       nei_parcel['neighbors_PARCEL_SQFT']= [self.zone_df['PARCEL_SQFT'].iloc[n].sum() for n in neighbors]
+
+       #calculate neiboring parcels percent with building_sqft>0
+       nei_parcel['neighbors_per_built']= [self.zone_df['BUILDING_SQFT'].iloc[n].map(lambda x: 1 if x>0 else 0).sum()/8.0 for n in neighbors]
+       
+       #calculate neiboring parcels total units
+       nei_parcel['neighbors_totalunits']= [self.zone_df['UNITS'].iloc[n].sum() for n in neighbors]
+       
+       self.zone_df['neighbors_FAR']=nei_parcel['neighbors_BUILDING_SQFT']/nei_parcel['neighbors_PARCEL_SQFT'].map(lambda x: x if x>0 else 1) 
+       self.zone_df['neighbors_bldsqft_per_unit']=nei_parcel['neighbors_BUILDING_SQFT']/nei_parcel['neighbors_totalunits'].map(lambda x: x if x>0 else 1)
+       self.zone_df['neighbors_unit_per_acre']=nei_parcel['neighbors_totalunits']*43560.0/nei_parcel['neighbors_PARCEL_SQFT'].map(lambda x: x if x>0 else 1)
+       
+       self.zone_df['neighbors_bldsqft_per_unit_n']=np.log(self.zone_df['neighbors_bldsqft_per_unit'].map(lambda x: 1 if (x==0) | (x is None) else x))/np.log(self.zone_df['neighbors_bldsqft_per_unit'].max())
+       self.zone_df['neighbors_age_n']=self.zone_df['neighbors_age']/(self.zone_df['neighbors_per_built'].map(lambda x: 1 if (x==0) | (x is None) else x)*8)
+       self.zone_df['neighbors_age_n']=self.zone_df['neighbors_age_n']/self.zone_df['neighbors_age_n'].max()
+   
+        
+       
 
 def main():
     test_model = model(sys.argv[1])
     test_model.allocate()
     test_model.update()
+    test_model.updateneighbor()
     test_model.zone_df.to_csv(sys.argv[2], index=False)
 
 if __name__=="__main__":
